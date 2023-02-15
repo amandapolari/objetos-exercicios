5764d77c7c0de266b468b5f85988adfc1bcfba42
Amanda <amandaoliveirapolari@gmail.com>
2023-02-15 18:32:32 -0300

    Exercício 1 -> ok!

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+const sacolao = [];
+
+const frutas = {
+    nome: 'morango'
+}
+
+const valor = {
+    preco: 12
+}
+
+const existencia = {
+    disponibilidade: true
+}
+
+sacolao.push(frutas,valor,existencia);
+console.log(sacolao);
